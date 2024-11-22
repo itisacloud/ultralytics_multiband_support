cc332a043e6a7c632f86912b5faac3136ea1ee47
clemens.langer@stud.uni.heidelberg.de <clemens.langer@stud.uni-heidelberg.de>
2024-11-22 14:45:06 +0100

    fixes

--- ultralytics_MB/nn/tasks.py
+++ ultralytics_MB/nn/tasks.py
@@ -342,7 +342,7 @@ class DetectionModel(BaseModel):
                     return self.forward(x)["one2many"]
                 return self.forward(x)[0] if isinstance(m, (Segment, Pose, OBB)) else self.forward(x)
             zeroes = torch.zeros(1, ch, s, s)
-            print(zeroes.size())
+            print("zeroes size",zeroes.size())
             m.stride = torch.tensor([s / x.shape[-2] for x in _forward(zeroes)])  # forward
             self.stride = m.stride
             m.bias_init()  # only run once
